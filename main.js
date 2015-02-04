0203dcbc42f0d0bde9f93903ccd474cc0f972fd1
Matt Rich <metropolis_4@hotmail.com>
2015-02-04 11:44:13 -0700

    Pulling out functions

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ var Quotes = function(author, quote){
     this.author = author;
     this.quote = quote;
 };
+var quotesConstructor = function(quoteList){
+    var quotes = quoteList.map(function(quote){
+        var $listAuthor = $('<h3>')
+            .text(quote.author);
+        var $listQuote = $('<div>')
+            .text('"' + quote.quote + '"');
+        var $listQuoteAuthor = $('<span>')
+            .addClass('author')
+            .text('  -' + quote.author);
+        $listQuote = $listQuote.append($listQuoteAuthor);
+        console.log($listQuote);
+
+        $('#accordion').append($listAuthor).append($listQuote);
+    });
+    return quotes;
+};
 
 $(document).on('ready', function() {
     var allQuotes = [];
@@ -18,20 +34,7 @@ $(document).on('ready', function() {
             var quoteText = $('#newQuote').val();
             var quote = new Quotes(author, quoteText);  
             allQuotes.push(quote);
-
-                allQuotes.map(function(quote){
-                    var $listAuthor = $('<h3>')
-                        .text(quote.author);
-                    var $listQuote = $('<div>')
-                        .text('"' + quote.quote + '"');
-                    var $listQuoteAuthor = $('<span>')
-                        .addClass('author')
-                        .text('  -' + quote.author);
-                    $listQuote = $listQuote.append($listQuoteAuthor);
-                    console.log($listQuote);
-                    $('#accordion').append($listAuthor).append($listQuote);
-                });
-
+            quotesConstructor(allQuotes);
             $('#accordion').accordion('destroy').accordion();
             $('.popup').modal('hide');
             $('#newQuoteAuthor').val('');
