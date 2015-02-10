0b5adb1b5a8d506db70a1d02688466bf3325006a
Matt Rich <metropolis_4@hotmail.com>
2015-02-10 16:52:22 -0700

    Functionality added

--- main.js
+++ main.js
@@ -1,45 +1,89 @@
-var Quotes = function(author, quote){
-    this.author = author;
-    this.quote = quote;
-};
-var quotesConstructor = function(quoteList){
-    var quotes = quoteList.map(function(quote){
-        var $listAuthor = $('<h3>')
-            .text(quote.author);
-        var $listQuote = $('<div>')
-            .text('"' + quote.quote + '"');
-        var $listQuoteAuthor = $('<span>')
-            .addClass('author')
-            .text('  -' + quote.author);
-        $listQuote = $listQuote.append($listQuoteAuthor);
-        console.log($listQuote);
+var Quotes = (function(){
+    var allQuotes = [];
 
-        $('#accordion').append($listAuthor).append($listQuote);
-    });
-    return quotes;
-};
+    var Quote = function(author, quote){
+        this.author = author;
+        this.quote = quote;
+    };
+
+    Quote.quoteList = allQuotes;
+
+    Quote.makeQuote = function(auth, text){
+        var quote = new Quote(auth, text);
+        allQuotes.push(quote);
+    };
+
+    Quote.rating = function(rating){
+        console.log(rating);
+        return Quote.starRating = rating;
+    };
+
+    Quote.makeDOM = function(quoteList, location){
+        var quotes = quoteList.map(function(quote){
+            var $listAuthor = $('<h3>')
+                .text(quote.author);
+            var $listQuote = $('<div>')
+                .text('"' + quote.quote + '"  -');
+            var $listQuoteAuthor = $('<span>')
+                .addClass('author')
+                .text(quote.author);
+            var makeStars = _.map([1,2,3,4,5], function(num) {
+                return $('<input type="radio" name="rating" value="' + num + '"><i></i>');
+            });
+            var ratingSys = $('<span>')
+                .addClass('star-rating')
+                .append(makeStars);
+            $listQuote = $listQuote.append($listQuoteAuthor).append(ratingSys);
+            $(location).append($listAuthor).append($listQuote);
+        });
+        return quotes;
+    };
+
+    return Quote;
+})();
 
 $(document).on('ready', function() {
-    var allQuotes = [];
-    $('#accordion').accordion();
+    
+    $('.main-accordion').accordion();
+    $('.sorted-accordion').accordion();
 
     $('.add-content').click(function(){
 
         $('.popup').modal('show');
 
-        $('.post-content').click(function(e){
+        $(document).on('click', '.post-content', function(e){
             e.preventDefault();
-            $('#accordion').empty();
-            var author = $('#newQuoteAuthor').val();
-            var quoteText = $('#newQuote').val();
-            var quote = new Quotes(author, quoteText);  
-            allQuotes.push(quote);
-            quotesConstructor(allQuotes);
-            $('#accordion').accordion('destroy').accordion();
+            $('.main-accordion').empty();
+            Quotes.makeQuote($('#newQuoteAuthor').val(), $('#newQuote').val()); 
+            Quotes.makeDOM(Quotes.quoteList, '.main-accordion');
+            $('.main-accordion').accordion('destroy').accordion();
             $('.popup').modal('hide');
             $('#newQuoteAuthor').val('');
             $('#newQuote').val('');
+
+            $('input[name="rating"]').click(function() {
+                Quotes.rating(this.value);
+            });
+        });
+
+    });
+    
+    
+
+
+    $(document).on('click', '.author', function(){
+        $('.sorted-accordion').empty();
+        $('.sort-pop').modal('show');
+        var authorName = ($(this).text());
+        var sortedQuotes = _.filter(Quotes.quoteList, function(val){
+            return val.author === authorName;
+        });
+        Quotes.makeDOM(sortedQuotes, '.sorted-accordion');
+
+        $('.close-sorted-list').click(function(){
+            $('.sort-pop').modal('hide');
         });
     });
+
 
 });
